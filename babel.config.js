7964396123a6e12f70551b0c5e2c536df75b59a1
MML77totti <103061608+MML77totti@users.noreply.github.com>
2023-06-17 15:53:09 -0300

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo'],
+//     plugins: [
+//       "@babel/plugin-proposal-export-namespace-from",
+//       "react-native-reanimated/plugin",
+//     ],
+//   };
+// };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+    ],
+  };
+};
